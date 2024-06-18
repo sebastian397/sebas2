9c2222253eda85e3ee84375aa5b1a956dfb7b867
sebastian397 <157639840+sebastian397@users.noreply.github.com>
2024-06-18 14:05:53 -0600

    Update admin.py

--- chat/admin.py
+++ chat/admin.py
@@ -7,3 +7,15 @@ from .models import Mensaje
 from .models import Carrera
 from .models import Persona
 
+@admin.register(Carrera)
+class CarreraAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'status')
+
+@admin.register(Persona)
+class PersonaAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'ap_pat', 'ap_mat', 'usu', 'fecha_nac', 'carrera', 'status')
+
+@admin.register(Mensaje)
+class MensajeAdmin(admin.ModelAdmin):
+    list_display = ('txt_mensaje', 'persona', 'status')
+
